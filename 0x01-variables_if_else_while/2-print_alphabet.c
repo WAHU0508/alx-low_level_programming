1229950c1656921e076179bb736bfd513b9385e0
WAHU0508 <sharongikenye@gmail.com>
2022-09-10 01:50:28 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main-print alphabets
+ *
+ * Return: Always 0(SUCCESS)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
