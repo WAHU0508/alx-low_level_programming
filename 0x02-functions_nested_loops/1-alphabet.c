2aa37ee80af054e7417980d281748b295eee7364
WAHU0508 <sharongikenye@gmail.com>
2022-09-13 06:44:22 -0700

    Task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - print alphabets in lower case
+ *
+ * Return: 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
