013826262c643bdf18e139a62936a540315020a1
WAHU0508 <sharongikenye@gmail.com>
2022-09-26 21:39:33 +0300

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,7 +13,7 @@ char *_strchr(char *s, char c)
 	int i = 0;
 
 	while (s[i] != '\0' && s[i] != c) /* find match */
-		I++;
+		i++;
 
 	if (s[i] == c) /* if match, assign to address */
 		return (&s[i]);
