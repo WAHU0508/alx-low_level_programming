deb89b874479ef311a9063e9c2f2bbb7411d0566
WAHU0508 <sharongikenye@gmail.com>
2022-09-28 20:08:32 +0300

    Task 0

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,2 @@
+0. Write a function that prints a string, followed by a new line.
+
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H_
+#define _MAIH_H
+
+void _puts_recursion(char *s);
+int _putchar(char c);
+
+#endif
